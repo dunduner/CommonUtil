7e5fe9fdcdd893d140301eddd8c10a84ccd4eaa4
sunning <ning11.zhang@zhangmen.com>
2021-01-21 15:53:11 +0800

    enen

--- src/main/java/io/github/sunning/asserts/Assert.java
+++ src/main/java/io/github/sunning/asserts/Assert.java
@@ -14,9 +14,6 @@ import java.util.Objects;
  */
 
 public class Assert {
-    public static void main(String[] args) {
-        System.out.println("最新修改！！！！");
-    }
     public static void state(boolean expression, String message) {
         if (!expression) {
             throw new IllegalStateException(message);

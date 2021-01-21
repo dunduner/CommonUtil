4bae585be9820c6872839baad627353e0d0bac3a
sunning <ning11.zhang@zhangmen.com>
2021-01-21 15:30:08 +0800

    demo1 2

---
+++ src/main/java/io/github/sunning/asserts/Demo1.java
@@ -0,0 +1,12 @@
+package io.github.sunning.asserts;
+
+/**
+ * @author ning11.zhang
+ * @Description:
+ * @date 2021/1/21
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
